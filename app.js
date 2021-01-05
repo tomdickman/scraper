0494a9ddacc005be62d4f61b07ef70f4d49e252e
Tom Dickman <twdickman@gmail.com>
2021-01-05 15:10:52 +1100

    :bug: Use puppeteer without sandbox
    - This avoids issues created in linux containers
    with Chrome Linux sandbox
    - TODO: replace this with user namespace cloning
    (not supported in node:12 docker images)

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const puppeteer = require("puppeteer")
 
 /**
- * Get the DOM for a URL (as a jQuery like DOM object for reading data).
+ * Get the content of a rendered web page.
  * 
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  * @param {Object} context
  * 
- * @return {string} JSON encoded Cheerio with loaded html elements.
+ * @return {Object} JSON encoded response object with status code and extracted content or error message.
  */
 exports.lambdaHandler = async (event, context) => {
     const targetUrl = event.queryStringParameters.targeturl
 
     if (targetUrl !== undefined) {
         try {
-            browser = await puppeteer.launch({headless: true})
+            browser = await puppeteer.launch({args: ['--no-sandbox'], headless: true})
             page = await browser.newPage()
             await page.setUserAgent("Lambda scraper")
             await page.goto(targetUrl)
@@ -26,6 +26,11 @@ exports.lambdaHandler = async (event, context) => {
             }
         } catch(error) {
             console.log(error)
+        }
+    } else {
+        return {
+            'statusCode': 400,
+            'body': 'No valid URL provided'
         }
     }
 }
